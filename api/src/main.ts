aa089a8ad5047e2bdb49159fd3bcb7a7bc1e0c4d
T.Thuan <t.thuan@Fin-DungNTK.hungthinhcorp.com>
2022-12-16 14:16:15 +0700

    Fix Cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from '@app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.enableCors();
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
